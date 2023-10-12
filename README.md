70f549a92870b22a75689110860eed7f28c612ba
ricky.ltp <ricky.ltp@byteuptech.com>
2023-10-12 11:56:36 +0800

    Setup API Gateway

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 - **Notification Service** - Can send notifications, after order is placed
 - **Order Service**, **Inventory Service** and **Notification Service** are going to interact with each other
 - Synchronous and Asynchronous communication
+
+#### Create image docker keycloak
+docker run -p 8181:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:22.0.4 start-dev
+
+#### Refer document
+[spring-cloud-gateway-eureka-service-discovery](https://github.com/BarathArivazhagan/spring-cloud-gateway-eureka-service-discovery/blob/master/docker-compose.yml)
---
+++ discovery-server/src/main/java/com/phucdevs/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.phucdevs.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+//@SuppressWarnings("deprecation")
+//@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Value("${eureka.username}")
+    private String username;
+    
+    @Value("${eureka.password}")
+    private String password;
+    
+    @Override
+    public void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+        
+        managerBuilder.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(username)
+                .password(password)
+                .authorities("USER");
+    }
+    
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
